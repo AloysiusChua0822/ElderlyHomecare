38c3349567d254950c718d702b858fc0ca85c247
yahya <yahya2002.j@gmail.com>
2024-02-17 11:53:32 +0800

    Community+Appointments

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -50,8 +50,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
           'image_url': _profilePicUrl?.path, // Store the file path
         });
 
-
-
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Profile updated successfully!')),
         );
@@ -75,16 +73,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
-        actions: _isUpdated ? [
+        actions: _isUpdated
+            ? [
           IconButton(
             icon: Icon(Icons.save),
             onPressed: _updateUserProfile,
           )
-        ] : null,
+        ]
+            : null,
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             OldImagePicker(),
             SizedBox(height: 20),
@@ -93,6 +94,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
             _buildPasswordField(),
             _buildUserTypeField(),
             SizedBox(height: 20),
+            if (user != null)
+              MedicationTaskCard(
+                tasks: 7, // This should be retrieved from your data
+                progress: 0.72, // This should also be retrieved from your data
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MedicationScreen(userId: user!.uid, currentUserType: _userType),
+                    ),
+                  );
+                },
+              ),
           ],
         ),
       ),
@@ -113,8 +127,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: TextField(
               controller: _usernameController,
               readOnly: true,
-              onChanged: (value) {
-              },
+              onChanged: (value) {},
               decoration: InputDecoration(
                 border: InputBorder.none,
               ),
@@ -130,6 +143,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
+
   void _editUsername() {
     showDialog(
       context: context,
@@ -246,13 +260,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
             },
             child: Text('Save'),
           ),
-
         ],
       ),
     );
   }
-
-
 
   Widget _buildUserTypeField() {
     return Container(
@@ -274,6 +285,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
+
   Widget _buildEmailField() {
     return Container(
       margin: EdgeInsets.only(bottom: 10),
@@ -291,6 +303,257 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+
+
+
+class MedicationScreen extends StatefulWidget {
+  final String userId;
+  final String currentUserType; // Add currentUserType parameter
+
+  MedicationScreen({required this.userId, required this.currentUserType});
+
+  @override
+  _MedicationScreenState createState() => _MedicationScreenState();
+}
+
+class _MedicationScreenState extends State<MedicationScreen> {
+  late List<DocumentSnapshot> users;
+  late TextEditingController _medicationController;
+  late String selectedUserId;
+
+  @override
+  void initState() {
+    super.initState();
+    _medicationController = TextEditingController();
+    selectedUserId = ''; // Initialize selectedUserId
+    _fetchUsers(); // Fetch users when the widget initializes
+  }
+
+  void _fetchUsers() async {
+    // Fetch all users from Firestore
+    QuerySnapshot querySnapshot =
+    await FirebaseFirestore.instance.collection('users').get();
+    setState(() {
+      users = querySnapshot.docs;
+    });
+  }
+
+  void _loadMedicationDetails(String userId) async {
+    try {
+      // Load medication details for the selected user ID from Firestore
+      DocumentSnapshot snapshot =
+      await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+      // Check if the document exists and if it contains the 'medications' field
+      if (snapshot.exists && snapshot.data() != null && (snapshot.data()! as Map<String, dynamic>).containsKey('medications')) {
+        // Use setState only if this is within a StatefulWidget
+        setState(() {
+          // Safely try to access 'medications' within the document data.
+          // If the 'medications' field exists, set the medication text to its value.
+          // Otherwise, set it to 'No medications found'.
+          final medications = (snapshot.data()! as Map<String, dynamic>)['medications'];
+          _medicationController.text = medications is String ? medications : 'No medications found';
+        });
+      } else {
+        setState(() {
+          // Set a default message if the 'medications' field does not exist
+          _medicationController.text = 'No medications found';
+        });
+      }
+    } catch (e) {
+      // If there's an error (e.g., the document doesn't exist), catch it and set a default message
+      print('An error occurred while fetching medications: $e');
+      setState(() {
+        _medicationController.text = 'Error loading medications';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Medication Details'),
+        actions: widget.currentUserType ==
+            'Health Personnel' // Only show save button for healthcare personnel
+            ? [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              _saveMedicationDetails(selectedUserId);
+            },
+          ),
+        ]
+            : null,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (users != null)
+            Expanded(
+              child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  var user = users[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(user['image_url'] ?? ''),
+                    ),
+                    title: Text(user['username']),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserDetailScreen(
+                            userId: user.id,
+                            userName: user['username'],
+                            medication: user['medications'] ?? '',
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name: ${_medicationController.text}',
+                  // Adjusted to show the selected user's name
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'medications:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _medicationController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  readOnly: widget.currentUserType != 'Health Personnel',
+                  // Make medication details read-only if not healthcare personnel
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter medication details...',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _saveMedicationDetails(String userId) async {
+    // Save medication details to Firestore for the selected user ID
+    if (widget.currentUserType == 'Health Personnel') {
+      await FirebaseFirestore.instance.collection('users').doc(userId).update({
+        'medications': _medicationController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Medication details saved successfully!')),
+      );
+    }
+  }
+}
+
+
+
+class MedicationTaskCard extends StatelessWidget {
+  final int tasks;
+  final double progress;
+  final VoidCallback onTap;
+
+  const MedicationTaskCard({
+    Key? key,
+    required this.tasks,
+    required this.progress,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        color: Colors.teal[100],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              CircularProgressIndicator(
+                value: progress, // Updated to use the progress parameter
+                backgroundColor: Colors.grey.shade300,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Document medication',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '$tasks Tasks', // Updated to use the tasks parameter
+                style: TextStyle(fontSize: 14),
+              ),
+              Icon(Icons.arrow_forward_ios),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UserDetailScreen extends StatelessWidget {
+  final String userId;
+  final String userName;
+  final String medication;
+
+  const UserDetailScreen({
+    Key? key,
+    required this.userId,
+    required this.userName,
+    required this.medication,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Detail'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Name: $userName',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            // Add code to display user's photo here if needed
+            SizedBox(height: 10),
+            Text(
+              'Medication: $medication',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
